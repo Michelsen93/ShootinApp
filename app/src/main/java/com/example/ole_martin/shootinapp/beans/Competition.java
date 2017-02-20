f3379c48c1255374285ac85d1c4cd41d97bb3c4d
Michelsen93 <om.michelsen@gmail.com>
2017-02-20 11:16:41 +0100

    initial class commit

---
+++ app/src/main/java/com/example/ole_martin/shootinapp/beans/Competition.java
@@ -0,0 +1,11 @@
+package com.example.ole_martin.shootinapp.beans;
+
+import java.util.List;
+
+/**
+ * Created by ole-martin on 20.02.2017.
+ */
+
+public class Competition {
+    List<RoundRules> roundRules;
+}
